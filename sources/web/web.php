8654de0e81286924d129e26964b897830e7e50fb
Stephane R <robin.stephane@outlook.com>
2023-06-30 20:31:02 +0200

    Add files via upload

---
+++ sources/web/web.php
@@ -0,0 +1,51 @@
+<h2>Les langages du Web</h2>
+
+<!-- Bien débuter -->
+<div class='gridContainerTableMatieres'>
+
+    <div class="gridItemTableMatieres">
+        <img class="image_index" src="../../images/general/index14.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Bien débuter en HTML</h2>
+        <p>HTML et CSS sont des langages de communication largement utilisés sur le Web, le premier
+             pour le contenu du site et sa mise en page, le second pour le style des pages. 
+            Bien maîtriser ces deux langages est indispensable à la réalisation de n'importe quel 
+        site Web.</p>
+        <p>Vous découvrirez la structure HTML d'une page Web, les techniques pour positionner
+            correctement les différents blocs d'une page, puis finalement quelques CSS tricks qui
+            embellissent votre site.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "web", "debuter"); ?>
+    </div>
+
+</div>
+
+<!-- JavaScript -->
+<div class='gridContainerTableMatieres'>
+
+    <div class="gridItemTableMatieres">
+        <img class="image_index" src="../../images/general/index15.jpg" width=80%>
+    </div> 
+
+    <div class='gridItemTableMatieres'>
+        <h2>JavaScript</h2>
+        <p>JavaScript est plus particulièrement utilisé pour rendre le site Web interactif 
+            et fournir une expérience utilisateur agréable. Il est devenu un langage incontournable 
+        lors de la création de sites Web.</p>        
+        <p>Vous découvrirez les instructions de base qui permettent d'exécuter du JavaScript comme 
+            n'importe quel autre langage, puis les instructions spécifiques à la création de sites wen.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "web", "javascript"); ?>
+    </div>
+
+</div>
+
+    
