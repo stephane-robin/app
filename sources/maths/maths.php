66b7e08c36b7d28e269172b7ba55dff1a4c9ba8a
Stephane R <robin.stephane@outlook.com>
2023-06-30 17:40:45 +0200

    Add files via upload

---
+++ sources/maths/maths.php
@@ -0,0 +1,108 @@
+<!-------------------------->
+<!-- OUTILS MATHEMATIQUES -->
+<!-------------------------->
+<h2>Calcul numérique ...</h2>
+
+      
+<!-- statistiques probabilites -->
+<div class='gridContainerTableMatieres'>
+
+    <div class='gridItemTableMatieres'>
+        <img class="image_index" src="../../images/general/index1.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Statistiques Probabilités</h2>
+        <p>Savoir utiliser médiane, moyenne, quartiles, étendue, écart-type, écart interquartile,savoir calculer le taux global d'évolution, utiliser les 
+                règles de probabilité.</p>
+        <p>Les statistiques étudient des phénomènes en procédant à la collecte de données, à leur analyse, leur traitement et leur interprétation.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "maths", "statistiques"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches mémo</h4>
+        <div class="bouton2">
+            <a href="../../documents/quartiles.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Calcul de quartiles
+            </a>
+        </div>
+    </div>
+
+</div>
+
+
+
+<!-- calcul -->
+<div class='gridContainerTableMatieres'>
+
+    <div class="gridItemTableMatieres">
+        <img class="image_index" src="../../images/general/index4.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Calculs de base</h2>
+        <p>Savoir effectuer les calculs de bases sans se tromper, manipuler les fractions, puissances, racines carrées, valeurs absolues, ...</p>
+        <p>Nous verrons les bases de calcul telles qu'elles sont enseignées au collège, lycée, cycle et gymnase.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "maths", "calcul"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches mémo</h4>
+        <div class="bouton2">
+            <a href="../../documents/fiche_memo_fractions1.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Fractions
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/fiche_memo_puissances.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Puissances
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/valeurAbsolue.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Valeurs absolues
+            </a>
+        </div>
+    </div>
+
+</div>
+
+
+<!-- calcul litteral -->
+<div class='gridContainerTableMatieres'>
+
+    <div class="gridItemTableMatieres">
+        <img class="image_index" src="../../images/general/index5.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Calcul littéral</h2>
+        <p>Savoir factoriser, développer, utiliser des identités remarquables, savoir résoudre des équations de degré 1, de degré 2, des inéquations,...</p>
+        <p>Le calcul littéral utilise à la fois des nombres et des lettres qui représentent des inconnues. Nous verrons comment modifier des expressions mathématiques pour effectuer des calculs plus facilement.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "maths", "calcul_litteral"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches mémo</h4>
+        <div class="bouton2">
+            <a href="../../documents/fiche_memo_equations_degre2.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Calcul littéral
+            </a>
+        </div>
+    </div>
+
+</div>
+
+  
