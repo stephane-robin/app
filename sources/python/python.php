39a50698ccc72b57a2cddbe019398de75ae5f71f
Stephane R <robin.stephane@outlook.com>
2023-06-30 20:31:45 +0200

    Add files via upload

---
+++ sources/python/python.php
@@ -0,0 +1,195 @@
+<!-------------------------->
+<!-- PROGRAMMATION PYTHON -->
+<!-------------------------->
+<h2>Programmation Python</h2>
+
+<!-- bien debuter en python -->
+<div class='gridContainerTableMatieres'>
+
+    <div class='gridItemTableMatieres'>
+        <img class="image_index" src="../../images/general/index6.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Bien débuter en Python</h2>
+        <p>Python est un langage de programmation interprété dont la philosophie repose sur la simplicité 
+            du code, la liberté du programmeur et l'implémentation de bonnes pratiques. Rien n'est fait 
+            pour empêcher le programmeur de mener une action, même si celle-ci semble inappropriée.</p>
+        <p>Vous découvrirez les bases de Python à l'aide d'exemples progressifs qui vous apprendront 
+            à manipuler les chaînes de caractères, les conditions, les boucles, les fonctions, ...</p>
+    </div> 
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "python", "debuter"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches mémo</h4>
+        <div class="bouton2">
+            <a href="../../documents/ficheMemoPython.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Python
+            </a>
+        </div>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches d'exercices</h4>
+        <div class="bouton2">
+            <a href="../../documents/SNT_exercice.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            SNT 2ème - Exercices
+            </a>
+        </div>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Evaluations</h4>
+        <div class="bouton2">
+            <a href="../../documents/evaluationPython.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            SNT 2ème - Evaluation 1
+            </a>
+        </div>
+    </div>
+
+</div>
+
+
+<!-- progresser en python -->
+<div class='gridContainerTableMatieres'>
+    
+    <div class='gridItemTableMatieres'>
+        <img class="image_index" src="../../images/general/index3.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Progresser en Python</h2>
+        <p>Le calcul numérique est souvent lié au calcul effectué par un programme suite à la modélisation 
+            d'un phénomène concret. Python est particulièrement utilisé dans les environnements scientifiques, pour la manipulation 
+            de données et en intelligence artificielle. On le retrouve maintenant de plus en plus dans les sites Web.</p>
+        <p>Vous apprendrez à manipuler des listes, à utiliser des bibliothèques spécifiques pour tracer des 
+            courbes, résoudre des équations et créer des fonctions statistiques. Vous découvrirez comment 
+            tester vos codes et gérer vos erreurs. Vous saurez lire et modifier un fichier distant, puis apprendrez
+            à utiliser une base de données.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "python", "progresser"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches d'exercices</h4>
+        <div class="bouton2">
+            <a href="../../documents/01Prem.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI - Epreuve pratique Bac 1
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/03Prem.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI - Epreuve pratique Bac 2
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/04Prem.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI - Epreuve pratique Bac 3
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/18Prem.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI - Epreuve pratique Bac 4
+            </a>
+        </div>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Evaluations</h4>
+        <div class="bouton2">
+            <a href="../../documents/DST_nsi_1ere.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI 1ère - Evaluation 1
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/EC_nsi_1ere.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI 1ère - Evaluation 2
+            </a>
+        </div>
+    </div>
+
+</div>
+
+
+<!-- python avance -->
+<div class='gridContainerTableMatieres'>
+    
+    <div class="gridItemTableMatieres">
+        <img class="image_index" src="../../images/general/index8.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Python avancé</h2>
+        <p>La programmation objet est une manière de programmer qui utilise un modèle proche de la réalité où 
+            on représente chaque élément en tant qu'objet au sein d'une classe. Chaque objet a des caractéristiques propres 
+        qu'on peut modifier.</p>
+        <p>Vous apprendrez à différencier les différents paradigmes de programmation. En particulier, 
+            vous saurez programmer en orienter objet, vous utiliserez la récursivité, et effectuerez des calculs 
+                complexes pour le référencement naturel, la cryptographie RSA, ...</p>    
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "python", "avance"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches d'exercices</h4>
+        <div class="bouton2">
+            <a href="../../documents/02Term.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI - Epreuve pratique Bac 5
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/05Term.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI - Epreuve pratique Bac 6
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/07Term.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI - Epreuve pratique Bac 7
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/16Term.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI - Epreuve pratique Bac 8
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/37Term.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">
+            NSI - Epreuve pratique Bac 9
+            </a>
+        </div>
+    </div>
+
+    
+
+</div>
+
+
