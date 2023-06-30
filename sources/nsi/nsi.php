5629e42ac410554d497022e383819ce053ad29aa
Stephane R <robin.stephane@outlook.com>
2023-06-30 19:28:57 +0200

    Add files via upload

---
+++ sources/nsi/nsi.php
@@ -0,0 +1,146 @@
+<!----------------------------
+  SCIENCES NUMERIQUES
+  ---------------------------->
+<h2>Sciences numériques</h2>
+
+<!-- internet -->
+<div class='gridContainerTableMatieres'>
+
+    <div class='gridItemTableMatieres'>
+        <img class="image_index" src="../../images/general/index2.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Internet, le Web, les réseaux sociaux</h2>
+        <p>Découvrir le modèle en couches, les protocoles, l'adresse IP, le Web, le DNS, les réseaux peer-to-peer, ...</p>
+        <p>Représenter les réseaux sous forme de graphes, analyser les dangers des réseaux sociaux.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "nsi", "internet"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches mémo</h4>
+        <div class="bouton2">
+            <a href="../../documents/memo_internet.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Internet
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/memo_web.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Requête sur le Web
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/memo_web2.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Web
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/memo_p2p.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Peer to peer
+            </a>
+        </div>
+        <div class="bouton2">
+            <a href="../../documents/memo_reseaux_sociaux.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Réseaux sociaux
+            </a>
+        </div>
+    </div>
+
+</div>
+
+
+<!-- structures de donnees -->
+<div class='gridContainerTableMatieres'> 
+
+    <div class='gridItemTableMatieres'>
+        <img class="image_index" src="../../images/general/index7.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Données structurées</h2>
+        <p>Découvrir les différents formats de données, l'algorithme du tri à bulles, le cloud computing, apprendre à récupérer des données à l'aide d'un script Python, ...</p>
+        <p>Python est particulièrement utilisé dans les environnements scientifiques, pour la manipulation de données et en intelligence artificielle.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "nsi", "donnees_structurees"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+    </div>
+
+</div>  
+
+
+<!-- objets connectes photographie numérique-->
+<div class='gridContainerTableMatieres'>   
+
+    <div class='gridItemTableMatieres'>
+        <img class="image_index" src="../../images/general/index12.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Objets connectés, photographie numérique</h2>
+        <p>Les objets connectés sont de plus en plus présents dans notre quotidien. Ils nécessitent
+            un accès Internet permanent et la programmation d'applications dédiés à leur fonctionnement.</p>   
+        <p>Comprendre la triangulation, la géolocalisation.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "nsi", "objets_connectes"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches mémo</h4>
+        <div class="bouton2">
+            <a href="../../documents/memo_objets_connectes.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Objets connectés
+            </a>
+        </div>
+    </div>
+
+</div>
+
+
+<!-- architecture materielle et reseaux -->
+<div class='gridContainerTableMatieres'> 
+
+    <div class="gridItemTableMatieres">
+        <img class="image_index" src="../../images/general/index13.jpg" width=80%>
+    </div>
+
+    <div class='gridItemTableMatieres'>
+        <h2>Architecture matérielle, réseaux</h2>
+        <p>Comprendre l'utilité des différents éléments composant un ordinateur. Savoir attribuer 
+            ume ressource à un processus. Connaître les principales lignes de commande du système 
+            d'exploitation Linux.</p>
+        <p>Comprendre la signification d'une adresse IP. Savoir construire une table de routage 
+            dans un réseau.</p>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Activités</h4>
+        <?php afficherTableMatieres($titres, "nsi", "architecture"); ?>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Fiches mémo</h4>
+        <div class="bouton2">
+            <a href="../../documents/memo_architecture_materielle.pdf" download>
+            <img src="../../images/general/pdfimg2.png" alt="télécharger pdf" width=20px style="margin-right:2%;">Fiche mémo - Architecture matérielle
+            </a>
+        </div>
+    </div>
+
+    <div class="gridItemTableMatieres navig_table_matiere">
+        <h4 class="blanc">Evaluations</h4>
+    </div>
+
+</div>
+
