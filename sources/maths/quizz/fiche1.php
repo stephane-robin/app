0b68922570216b4f6da6179b2b92daf0fb181585
Stephane R <robin.stephane@outlook.com>
2023-07-01 18:39:59 +0200

    Add files via upload

---
+++ sources/maths/quizz/fiche1.php
@@ -0,0 +1,34 @@
+<?php $compteurQuestion += 1; ?>
+<label for="q1"><em>Question <?php echo $compteurQuestion; ?> :</em> dans un triangle ABC tel que \(\hat{B}=33°\) et \(\hat{A}=15°\), peut-on appliquer le théorème de Pythagore ? </label><br/>
+
+<input style="margin-top:2%;" type="radio" id="q1r1" name="q1" value="False">
+<label style="margin-left:1%;" for="q1r1">oui</label><br/>
+<input type="radio" id="q1r2" name="q1" value="True">
+<label style="margin-left:1%;" for="q1r2">non</label><br/>
+<input style="margin-bottom:4%;" type="radio" id="q1r3" name="q1" value="False">
+<label style="margin-left:1%;" for="q1r3">ça dépend des longueurs des côtés du triangle</label><br/>
+
+<?php $compteurQuestion += 1; ?>
+<label for="q2"><em>Question <?php echo $compteurQuestion; ?> :</em> l'unique solution de \(x^2=5\) est \(\sqrt 5\) :</label><br/>
+
+<input style="margin-top:2%;" type="radio" id="q2r1" name="q2" value="True">
+<label style="margin-left:1%;" for="q2r1">faux</label><br/>
+<input type="radio" id="q2r2" name="q2" value="False">
+<label style="margin-left:1%;" for="q2r2">vrai</label><br/>
+<input style="margin-bottom:4%;" type="radio" id="q2r3" name="q2" value="False">
+<label style="margin-left:1%;" for="q2r3">\(\sqrt 5\) est une solution double</label><br/>
+
+<?php $compteurQuestion += 1; ?>
+<label for="q3" style="margin-top:2%;"><em>Question <?php echo $compteurQuestion; ?> :</em> en simplifiant \(2\sqrt 3 -7\sqrt 3+\sqrt 2\), on obtient :</label><br/> 
+
+<input style="margin-top:2%;" type="radio" id="q3r1" name="q3" value="False">
+<label style="margin-left:1%;" for="q3r1">\(-4 \sqrt 3\)</label><br/>
+<input type="radio" id="q3r2" name="q3" value="False">
+<label style="margin-left:1%;" for="q3r2">\(2\sqrt 2-7\sqrt 3\)</label><br/>
+<input style="margin-bottom:4%;" type="radio" id="q3r3" name="q3" value="True">
+<label style="margin-left:1%;" for="q3r3">\(-5\sqrt 3 + \sqrt 2\)</label><br/>
+ 	
+ 	
+
+
+
